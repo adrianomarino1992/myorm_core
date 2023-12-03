5821b01f7a91bbe55cdccdb29176e9f81582e163
adrianomarino1992 <adriano.marino1992@gmail.com>
2023-12-03 11:10:58 -0300

    abstractions improveds

--- core/objects/abstract/AbstractContext.ts
+++ core/objects/abstract/AbstractContext.ts
@@ -1,10 +1,10 @@
 import IDBContext, { IJoiningQuery, IThreeQueryableObject } from "../interfaces/IDBContext";
-import IDBSet from "../interfaces/IDBSet";
+import AbstractSet from "./AbastractSet";
 
 
 export default abstract class AbstractContext implements IDBContext , IThreeQueryableObject
 {
-    abstract Collection<T extends Object>(cTor: new (...args: any[]) => T): IDBSet<T>;
+    abstract Collection<T extends Object>(cTor: new (...args: any[]) => T): AbstractSet<T>;
     abstract UpdateDatabaseAsync(): Promise<void> ;
     abstract ExecuteNonQuery(query: string): Promise<void> ;
     abstract ExecuteQuery(query: string): Promise<any> ;
